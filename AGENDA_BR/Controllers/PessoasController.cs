daa5de7be3c1fad228e8bd39f72d7fdbbe88061a
leonidasc019 <leonidas19992011@hotmail.com>
2021-04-18 13:09:06 -0300

    Adicionar arquivos de projeto.

---
+++ AGENDA_BR/Controllers/PessoasController.cs
@@ -0,0 +1,111 @@
+﻿using AGENDA_BR.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+
+namespace AGENDA_BR.Controllers
+{
+    public class PessoasController : Controller
+    {
+        private readonly Contexto _contexto;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public PessoasController(Contexto contexto, IWebHostEnvironment webHostEnvironment)
+        {
+            _contexto = contexto;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            return View(await _contexto.Pessoas.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult AdicionarPessoa()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>AdicionarPessoa(Pessoa pessoa, IFormFile foto)
+        {
+            if(ModelState.IsValid)
+            {
+                if(foto != null)
+                {
+                    string diretorio = Path.Combine(_webHostEnvironment.WebRootPath, "imagens");
+                    string Fotonome = Guid.NewGuid().ToString() + foto.FileName;
+
+                    using(FileStream fileStream = new FileStream(Path.Combine(diretorio,Fotonome), FileMode.Create))
+                    {
+                        await foto.CopyToAsync(fileStream);
+                        pessoa.Foto = "~/imagens/" + Fotonome;
+                    }
+                }
+                else
+                {
+                    pessoa.Foto = "~/imagens/kisspng-computer-icons-user-clip-art-user-5abf13db298934.2968784715224718991702.jpg";
+                }
+                _contexto.Add(pessoa);
+                await _contexto.SaveChangesAsync();
+                TempData["Novo Contato"] = $"Contato incluido com sucesso";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pessoa);
+        }
+        [HttpGet]
+        public async Task<IActionResult> AtualizarPessoa(int pessoaId)
+        {
+            Pessoa pessoa = await _contexto.Pessoas.FindAsync(pessoaId);
+
+            if (pessoa == null)
+                return NotFound();
+
+            TempData["Foto"] = pessoa.Foto;
+            return View(pessoa);
+        }
+        [HttpPost]
+        public async Task<IActionResult>AtualizarPessoa(Pessoa pessoa, IFormFile foto)
+        {
+            if(ModelState.IsValid)
+            {
+               if(foto != null)
+                {
+                    string diretorio = Path.Combine(_webHostEnvironment.WebRootPath, "imagens");
+                    string Fotonome = Guid.NewGuid().ToString() + foto.FileName;
+                    using(FileStream fileStream = new FileStream(Path.Combine(diretorio, Fotonome),FileMode.Create))
+                    {
+                        await foto.CopyToAsync(fileStream);
+                        pessoa.Foto = "~/imagens/" + Fotonome;
+                        TempData["Foto"] = null;
+                    }
+                }
+               else
+                {
+                    pessoa.Foto = TempData["Foto"].ToString();
+                }
+                _contexto.Update(pessoa);
+                await _contexto.SaveChangesAsync();
+                TempData["ContatoAtualizado"] = "atualizado com sucesso";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> ExcluirPessoa(int pessoaId)
+        {
+            Pessoa pessoa = await _contexto.Pessoas.FindAsync(pessoaId);
+            _contexto.Pessoas.Remove(pessoa);
+            await _contexto.SaveChangesAsync();
+            TempData["ContatoExcluido"] = $"Contato {pessoa.Nome} {pessoa.Sobrenome} excluído com sucesso";
+            return Json(true);
+        }
+    }
+}
---
+++ AGENDA_BR/Models/Pessoa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AGENDA_BR.Models
+{
+    public class Pessoa
+    {
+        public int PessoaID { get; set; }
+        [Required(ErrorMessage ="O nome é um item obrigatório, favor preencher!")]
+        [StringLength(30, ErrorMessage = "Nome muito extenso.")]
+
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O Sobrenome é um item obrigatório, favor preencher!")]
+        [StringLength(30, ErrorMessage = "sobrenome muito extenso.")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "A idade é um item obrigatório, favor preencher!")]
+        [Range(18, int.MaxValue, ErrorMessage ="Idade inapropriada, favor rever.")]
+        public int Idade { get; set; }
+
+        [Required(ErrorMessage = "O email é um item obrigatório, favor preencher!")]
+        [StringLength(40, ErrorMessage = "Email muito extenso.")]
+        [EmailAddress (ErrorMessage ="Email inválido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O telefone é obrigatório.")]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage ="{0} é obrigatório")]
+        public tipoTelefone TipoTelefone { get; set; }
+
+        public string Foto { get; set; }
+    }
+    public enum tipoTelefone
+    { 
+       Publico,Presencial
+    }
+
+}
